15beb371d7249950c6cc8eaba1cfe9d72ed84509
hrome <58020216+hromero7@users.noreply.github.com>
2020-07-15 20:56:20 -0700

    uploading client folder structure

---
+++ fantasy-video-games/client/src/components/Jumbotron.js
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "../pages/assets/fvg.png";
+
+const Jumbotron = () => {
+    return (
+      <div className="jumbotron">
+      <h1 className="display-4">FVG</h1>
+      <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      <hr className="my-4"></hr>
+      <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
+      <p className="lead">
+        <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      </p>
+    </div>
+        
+    )
+}
+
+export default Jumbotron;
---
+++ fantasy-video-games/client/src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import NavBar from "../../components/NavBar";
+import Jumbotron from "../../components/Jumbotron";
+import "./style.css";
+
+const Home = () => {
+    return (
+    <div>
+    <NavBar/>
+    <Jumbotron/>
+    </div>
+    )
+}
+
+
+export default Home;
