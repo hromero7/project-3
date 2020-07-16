ee6cd1e1940073f5389080494456be9e3e883eb8
asedlack <asedlackcode@gmail.com>
2020-07-15 21:09:23 -0700

    socket

---
+++ fantasy-video-games/client/src/components/SignIn/SignIn.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import  { Link } from "react-router-dom";
+import "./SignIn.css"
+
+const SingIn = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+    function handleName(event) {
+        event.preventDefault();
+        setName(event.target.value)
+    }
+
+    function handleRoom(event) {
+        event.preventDefault();
+        setRoom(event.target.value)
+    }
+
+    return(
+        <div className="joinOuterContainer">
+            <div className="joinInnerContainer">
+                <h1 className="heading">Sign in</h1>
+                <div><input placeholder="Name" className="joinInput" type="text" onChange={handleName}/></div>
+                <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={handleRoom}/></div>
+                <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                <button className="button mt-20" type="submit">Sign In</button>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default SingIn;
--- fantasy-video-games/server.js
+++ fantasy-video-games/server.js
@@ -1,18 +1,40 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const socketio = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.PORT || 3003;
+
+const router = require("./routes/index")
 
 const app = express()
-const PORT = process.env.PORT || 3003;
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+  console.log("we have a new connection!!")
+
+  socket.on('disconnect', ()=> {
+    console.log("user has left")
+  })
+})
+
+app.use(router);
+
 
 mongoose.connect('mongodb://localhost/fantasyvideogames', {
   useNewUrlParser: true,
   useUnifiedTopology: true
+
+  
 });
 
 mongoose.connection.on('connected', () => {
   console.log("mongoose is connected")
 });
 
-app.listen(PORT, () => {
-    console.log(`🌎 ==> API server now on port ${PORT}!`);
-  });
+server.listen(PORT, () => console.log(`Server is running on ${PORT}`))
+
+// app.listen(PORT, () => {
+//     console.log(`🌎 ==> API server now on port ${PORT}!`);
+//   });
